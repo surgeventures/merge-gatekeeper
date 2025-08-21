f1082a53e7bb894baf54f5d73e978fe34ea64c60
rehan-fr <rehan.ullah@fresha.com>
2025-08-21 09:31:39 +0100

    [DEVX-1332] add required input (#13)
    * add required input
    
    * add printing of required job stasuses
    
    * add required jobs attr to class
    
    * add tests

--- internal/validators/status/option.go
+++ internal/validators/status/option.go
@@ -50,3 +50,23 @@ func WithIgnoredJobs(names string) Option {
 		s.ignoredJobs = jobs
 	}
 }
+
+func WithRequiredJobs(names string) Option {
+	return func(s *statusValidator) {
+		// TODO: Add more input validation, such as "," should not be a valid input.
+		if len(names) == 0 {
+			return // TODO: Return some clearer error
+		}
+
+		jobs := []string{}
+		ss := strings.Split(names, ",")
+		for _, s := range ss {
+			jobName := strings.TrimSpace(s)
+			if len(jobName) == 0 {
+				continue // TODO: Provide more clue to users
+			}
+			jobs = append(jobs, jobName)
+		}
+		s.requiredJobs = jobs
+	}
+}
